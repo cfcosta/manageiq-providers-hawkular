56f780578b3452fa5ce77e2b62f65fdb6e500721
Cainã Costa <cainan.costa@gmail.com>
2017-12-06 11:18:54 -0200

    Do not load engine if rails is not enabled
    This allows us to load the gem without having to load ManageIQ if
    necessary. Some behaviour is not going to be enabled like database
    persistence, but this is ok for testing GVM behaviour.

--- lib/manageiq/providers/hawkular/engine.rb
+++ lib/manageiq/providers/hawkular/engine.rb
@@ -1,8 +1,10 @@
-module ManageIQ
-  module Providers
-    module Hawkular
-      class Engine < ::Rails::Engine
-        isolate_namespace ManageIQ::Providers::Hawkular
+if defined?(Rails)
+  module ManageIQ
+    module Providers
+      module Hawkular
+        class Engine < ::Rails::Engine
+          isolate_namespace ManageIQ::Providers::Hawkular
+        end
       end
     end
   end
